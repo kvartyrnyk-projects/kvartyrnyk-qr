28c37362977109bb3d3ac645a3b55fadd28f589c
Alex Horyshevskyi <oleksii120@gmail.com>
2026-03-02 16:56:52 +0200

    chore: add simple app metadata

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { init } from "@tma.js/sdk-vue";
 
+useHead({
+  title: "Сканер QR-кодів для Квартирника",
+  meta: [
+    { name: "description", content: "Скануйте QR код для перевірки статусу" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+  ],
+});
+
 const route = useRoute();
 
 useMockTelegram();
