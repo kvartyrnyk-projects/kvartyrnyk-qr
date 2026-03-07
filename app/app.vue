98bd87aba7aa2654e30480729b4c5a296ed3b466
Alex Horyshevskyi <oleksii120@gmail.com>
2026-03-07 21:00:52 +0200

    fix: initialize Telegram Client SDK immediately

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { init } from "@tma.js/sdk-vue";
 
+if (import.meta.client) {
+  init();
+}
+
 useHead({
   title: "Сканер QR-кодів для Квартирника",
   meta: [
@@ -13,7 +17,6 @@ const route = useRoute();
 
 useMockTelegram();
 useOriginCheck(route.path);
-onMounted(init);
 </script>
 
 <template>
